9461c8bb397333ed308586ffc858515ae8922955
LaurelFR <128193405+LaurelFR@users.noreply.github.com>
2023-08-19 15:39:19 -0600

    Added functions to set red and orange

--- src/Board.js
+++ src/Board.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Board() {
+  const [red, setRed] = useState("red");
+  const [redPalette, setredPalette] = useState(false);
+  const [orange, setOrange] = useState("orange");
+  const [orangePalette, setorangePalette] = useState(false);
+
+  function handleRed() {
+    if (redPalette) {
+      setRed("red-active");
+    }
+  }
+
+  function handleRedClick() {
+    setredPalette(true);
+  }
+
+  function handleOrange() {
+    if (orangePalette) {
+      setOrange("orange-active");
+    }
+  }
+
+  function handleOrangeClick() {
+    setorangePalette(true);
+  }
+
   return (
     <div className="Board">
       <div className="picture mb-5">
-        <div className="red">
-          <div className="orange">
+        <div className={red} onClick={handleRed}>
+          1
+          <div className={orange} onClick={handleOrange}>
+            2
             <div className="yellow">
+              3
               <div className="green">
+                4
                 <div className="blue">
-                  <div className="violet"></div>
+                  5<div className="violet">6</div>
                 </div>
               </div>
             </div>
@@ -17,8 +46,14 @@ export default function Board() {
         </div>
       </div>
       <div className="palette d-flex justify-content-around">
-        <div className="palette-color palette-red"></div>
-        <div className="palette-color palette-orange"></div>
+        <div
+          className="palette-color palette-red"
+          onClick={handleRedClick}
+        ></div>
+        <div
+          className="palette-color palette-orange"
+          onClick={handleOrangeClick}
+        ></div>
         <div className="palette-color palette-yellow"></div>
         <div className="palette-color palette-green"></div>
         <div className="palette-color palette-blue"></div>
